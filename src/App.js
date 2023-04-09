ff80ef5cd5ef373b1b2f13d3f16a99cea4e9ea1d
Vanessa Man <thepurplesnail@github.com>
2023-04-09 12:13:14 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './styling/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className='title'>
+          Kono Annual Presitigious Awards
+        </p>
+        
+      </header>
+      <div>
+        HELLO WORLD
+      </div>
+    </div>
+  );
+}
+
+export default App;
