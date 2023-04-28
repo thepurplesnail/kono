9c5020e2c48ff968e9f64c29c159a4758f9885fc
Vanessa Man <thepurplesnail@github.com>
2023-04-28 00:36:08 -0400

    donations page outline done

--- src/pages/donations.jsx
+++ src/pages/donations.jsx
@@ -12,8 +12,8 @@ export const Donations = () => {
             <div className = 'header'>
                 <h1 className="title">Donations</h1>
             </div>
-            <div className="row-1">
-                <div className="card-wrapper">
+            <div className="row">
+                <div className="card-wrapper education">
                     <h2>Education</h2>
                     <div className="sub-categories" >
                             <h3>Universities</h3>
@@ -24,12 +24,26 @@ export const Donations = () => {
                     </div>
                 </div>
 
-                <div className="card-wrapper">
+                <div className="card-wrapper medical">
                     <h2>Medical</h2>
                     <div className="sub-categories">
                         <h3>Patients</h3>
                         <h3>Hospitals</h3>
                     </div>
+                </div>
+                <div className="card-wrapper orphanages single">
+                    <h3>Orphanages</h3>
+                </div>
+            </div>
+            <div className="row">
+                <div className="card-wrapper the-blinds single">
+                    <h3>The Blinds</h3>
+                </div>
+                <div className="card-wrapper entertainment single">
+                    <h3>Entertainments</h3>
+                </div>
+                <div className="card-wrapper soccer single">
+                    <h3>Soccer: Ibrahim Dainkeh Memorial Gala</h3>
                 </div>
             </div>
         </div>
